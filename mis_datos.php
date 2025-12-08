4ec8e67d25462bb0402aa2ce4e4709b2b31d9709
achavana <169084372+achavana@users.noreply.github.com>
2025-12-08 16:31:01 +0000

    Primeros pasos

---
+++ mis_datos.php
@@ -0,0 +1,9 @@
+<?php
+    $nombres = "Miguel Angel";
+    $apellidos = "Sámchez Chavana";
+    $correo = "achavana@gmail.com";
+    $cargo = "Especialista en Soporte Informático";
+    $domicilio = "CDMX";
+
+    $nombre_completo = $nombres . " " . $apellidos;
+?>
